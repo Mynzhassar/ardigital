3a342eadbfeb677b131835815d859d0e92f50272
dias.mynzhasar <d.mynzhasar@aviata.me>
2021-03-23 12:38:27 +0600

    used django-workers

--- backend/code/settings.py
+++ backend/code/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'code',
+    'workers',
 ]
 
 MIDDLEWARE = [
--- backend/code/tasks.py
+++ backend/code/tasks.py
@@ -1,19 +1,17 @@
-import os
-import django
 import pytz
-import time
 
 from datetime import datetime
+from workers import task
+
+from . import models, constants, notifications, utils
 
 
 def send_notification_to_admin(cur_datetime):
-    from . import models, constants, notifications, utils
-
     if cur_datetime.hour not in constants.HOURS_TO_SEND_EMAIL:
         return
 
-    consultations = models.Consultation.objects.filter(was_sent=False)
-    applications = models.Application.objects.filter(was_sent=False)
+    consultations = models.Consultation.objects.filter(was_notified=False)
+    applications = models.Application.objects.filter(was_notified=False)
 
     if not consultations and not applications:
         return
@@ -22,10 +20,11 @@ def send_notification_to_admin(cur_datetime):
         utils.generate_admin_text(cur_datetime, consultations.count(), applications.count()),
         constants.ARDIGITAL_EMAIL)
 
-    consultations.update(was_sent=True)
-    applications.update(was_sent=True)
+    consultations.update(was_notified=True)
+    applications.update(was_notified=True)
 
 
+@task(schedule=constants.TEN_MINUTES)
 def run_worker():
     while True:
         try:
@@ -33,11 +32,3 @@ def run_worker():
             send_notification_to_admin(cur_datetime)
         except Exception as e:
             print(str(e))
-
-        time.sleep(60 * 60 * 60)
-
-
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-    django.setup()
-    run_worker()
