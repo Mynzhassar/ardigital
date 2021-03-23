b906ebc1af0fd0258f777317a3890adb87706d36
dias.mynzhasar <d.mynzhasar@aviata.me>
2021-03-23 19:59:33 +0600

    add: check if was notified

--- backend/code/tasks.py
+++ backend/code/tasks.py
@@ -1,14 +1,25 @@
 import pytz
+import redis
 
 from datetime import datetime
 from workers import task
 
 from . import models, constants, notifications, utils
 
+redis_db = redis.Redis()
+
 
 def send_notification_to_admin(cur_datetime):
     if cur_datetime.hour not in constants.HOURS_TO_SEND_EMAIL:
         return
+
+    redis_key = cur_datetime.strftime(constants.REDIS_KEY_DATETIME_FORMAT)
+    was_notified = redis_db.get(redis_key)
+
+    if was_notified:
+        return
+
+    redis_db.setex(redis_key, constants.REDIS_KEY_TTL, 1)
 
     consultations = models.Consultation.objects.filter(was_notified=False)
     applications = models.Application.objects.filter(was_notified=False)
