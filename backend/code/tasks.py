7b443b01d70cc7cd4155a4ccd20564a30a44d8db
dias.mynzhasar <d.mynzhasar@aviata.me>
2021-03-24 16:48:56 +0600

    rm: redis, backend is ready

--- backend/code/tasks.py
+++ backend/code/tasks.py
@@ -1,25 +1,14 @@
 import pytz
-import redis
 
 from datetime import datetime
 from workers import task
 
 from . import models, constants, notifications, utils
 
-redis_db = redis.Redis()
-
 
 def send_notification_to_admin(cur_datetime):
     if cur_datetime.hour not in constants.HOURS_TO_SEND_EMAIL:
         return
-
-    redis_key = cur_datetime.strftime(constants.REDIS_KEY_DATETIME_FORMAT)
-    was_notified = redis_db.get(redis_key)
-
-    if was_notified:
-        return
-
-    redis_db.setex(redis_key, constants.REDIS_KEY_TTL, 1)
 
     consultations = models.Consultation.objects.filter(was_notified=False)
     applications = models.Application.objects.filter(was_notified=False)
@@ -35,7 +24,7 @@ def send_notification_to_admin(cur_datetime):
     applications.update(was_notified=True)
 
 
-@task(schedule=constants.TEN_MINUTES)
+@task(schedule=constants.ONE_HOUR)
 def run_worker():
     try:
         cur_datetime = datetime.utcnow().astimezone(pytz.timezone('Asia/Almaty'))
