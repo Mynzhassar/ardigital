8b5208096ba60d4e3db53a2958b41defcaab613e
dias.mynzhasar <d.mynzhasar@aviata.me>
2021-03-28 13:41:41 +0600

    reconstructed to add client side

--- backend/code/settings.py
+++ backend/code/settings.py
@@ -60,6 +60,20 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
