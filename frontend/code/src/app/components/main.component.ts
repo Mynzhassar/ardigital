364a58de03159cb47af7a3ddfa7d60114513de6e
dias.mynzhasar <d.mynzhasar@aviata.me>
2021-05-07 19:56:23 +0600

    applying logic in frontend

--- frontend/code/src/app/components/main.component.ts
+++ frontend/code/src/app/components/main.component.ts
@@ -3,6 +3,7 @@ import {ProviderService} from '../services/provider.service';
 import {Advertisement, Application, Consultation, Profit, Service, Site} from '../models/models';
 import {Observable, Subject} from "rxjs";
 import {takeUntil} from "rxjs/operators";
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-main',
@@ -13,32 +14,34 @@ import {takeUntil} from "rxjs/operators";
 
 export class MainComponent implements OnInit, OnDestroy {
 
-  public API_URL = "http://localhost:80";
+  public apiUrl = environment.apiUrl;
 
   public profits: Observable<Profit[]> | undefined;
   public services: Observable<Service[]> | undefined;
-  public sites: Observable<Site[]> | undefined
+  public sites: Observable<Site[]> | undefined;
   public advertisements: Observable<Advertisement[]> | undefined;
-  public applications: Application[] = []
-  public consultations: Consultation[] = []
+  public applications: Application[] = [];
+  public consultations: Consultation[] = [];
 
-  public consultation_full_name: any = ''
-  public consultation_telephone_number: any = ''
-  public consultation_email: any = ''
+  public consultationFullName: any = '';
+  public consultationTelephoneNumber: any = '';
+  public consultationEmail: any = '';
 
-  public application_full_name: any = ''
-  public application_telephone_number: any = ''
-  public application_email: any = ''
+  public applicationFullName: any = '';
+  public applicationTelephoneNumber: any = '';
+  public applicationEmail: any = '';
+
+  public showCase: any = 'sites';
 
   private destroy = new Subject<void>();
+
   constructor(private provider: ProviderService) {
   }
 
   ngOnInit() {
-    this.getProfits()
-    this.getServices()
-    this.getSites()
-    this.getAdvertisements()
+    this.getProfits();
+    this.getServices();
+    this.getCases(this.showCase);
   }
 
   public getProfits() {
@@ -50,11 +53,11 @@ export class MainComponent implements OnInit, OnDestroy {
   }
 
   public addConsultation(service_id: number) {
-    this.provider.addConsultation(service_id, this.consultation_full_name, this.consultation_telephone_number, this.consultation_email).pipe(takeUntil(this.destroy)).subscribe(res => {
-      this.consultation_full_name = ''
-      this.consultation_telephone_number = ''
-      this.consultation_email = ''
-      this.consultations.push(res)
+    this.provider.addConsultation(service_id, this.consultationFullName, this.consultationTelephoneNumber, this.consultationEmail).pipe(takeUntil(this.destroy)).subscribe(res => {
+      this.consultationFullName = '';
+      this.consultationTelephoneNumber = '';
+      this.consultationEmail = '';
+      this.consultations.push(res);
     })
   }
 
@@ -66,11 +69,30 @@ export class MainComponent implements OnInit, OnDestroy {
     this.advertisements = this.provider.getAdvertisements();
   }
 
+  public getCases(current_case: string) {
+    if (current_case == 'sites') {
+      this.getSites();
+    }
+    if (current_case == 'advertisements') {
+      this.getAdvertisements();
+    }
+  }
+
+  public showSites() {
+    this.showCase = 'sites';
+    this.getCases(this.showCase);
+  }
+
+  public showAdvertisements() {
+    this.showCase = 'advertisements';
+    this.getCases(this.showCase);
+  }
+
   public addApplication() {
-    this.provider.addApplication(this.application_full_name, this.application_telephone_number, this.application_email).pipe(takeUntil(this.destroy)).subscribe(res => {
-      this.application_full_name = ''
-      this.application_telephone_number = ''
-      this.application_email = ''
+    this.provider.addApplication(this.applicationFullName, this.applicationTelephoneNumber, this.applicationEmail).pipe(takeUntil(this.destroy)).subscribe(res => {
+      this.applicationFullName = '';
+      this.applicationTelephoneNumber = '';
+      this.applicationEmail = '';
       this.applications.push(res)
     })
   }
--- frontend/code/src/app/services/provider.service.ts
+++ frontend/code/src/app/services/provider.service.ts
@@ -2,45 +2,46 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import type {Advertisement, Application, Consultation, Profit, Service, Site} from '../models/models';
 import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProviderService {
-  API_URL = 'http://localhost:80/api'
+  apiUrl = environment.apiUrl + '/api';
 
   constructor(private http: HttpClient) {
   }
 
   public getProfits(): Observable<Profit[]> {
-    return this.http.get<Profit[]>(`${this.API_URL}/profits`)
+    return this.http.get<Profit[]>(`${this.apiUrl}/profits`);
   }
 
   public getServices(): Observable<Service[]> {
-    return this.http.get<Service[]>(`${this.API_URL}/services`)
+    return this.http.get<Service[]>(`${this.apiUrl}/services`);
   }
 
-  public addConsultation(service_id: number, full_name: any, telephone_number: any, email: any): Observable<Consultation> {
-    return this.http.post<Consultation>(`${this.API_URL}/${service_id}/add_consultation`, {
-      full_name: full_name,
-      telephone_number: telephone_number,
+  public addConsultation(serviceId: number, fullName: any, telephoneNumber: any, email: any): Observable<Consultation> {
+    return this.http.post<Consultation>(`${this.apiUrl}/${serviceId}/add_consultation`, {
+      fullName: fullName,
+      telephoneNumber: telephoneNumber,
       email: email,
-    })
+    });
   }
 
   public getSites(): Observable<Site[]> {
-    return this.http.get<Site[]>(`${this.API_URL}/sites`, {})
+    return this.http.get<Site[]>(`${this.apiUrl}/sites`, {});
   }
 
   public getAdvertisements(): Observable<Advertisement[]> {
-    return this.http.get<Advertisement[]>(`${this.API_URL}/advertisements`, {})
+    return this.http.get<Advertisement[]>(`${this.apiUrl}/advertisements`, {});
   }
 
-  public addApplication(full_name: any, telephone_number: any, email: any): Observable<Application> {
-    return this.http.post<Application>(`${this.API_URL}/add_application`, {
-      full_name: full_name,
-      telephone_number: telephone_number,
+  public addApplication(fullName: any, telephoneNumber: any, email: any): Observable<Application> {
+    return this.http.post<Application>(`${this.apiUrl}/add_application`, {
+      full_name: fullName,
+      telephone_number: telephoneNumber,
       email: email,
-    })
+    });
   }
 }
